d50ea1f2ba85c45645033b7660d3e655a912d680
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-28 23:39:12 -0800

    add readline on add customer method

--- CafeUtil.java
+++ CafeUtil.java
@@ -1,3 +1,6 @@
+import java.io.Console;
+import java.util.ArrayList;
+
 // ///////////////////////////////////////////////////////////////////
 //  CAFE UTIL
 //  By: Virgilio D. Cabading Jr.    Created at: November 28, 2021 2145
@@ -20,7 +23,7 @@ public class CafeUtil {
         return sum;
     }
 
-    //  //// Get Streak Goal | @param int numWeeks ////s////////////////
+    //  //// Get Streak Goal | @param int numWeeks ///////////////////
     //  Each week, a customer needs to purchase 1 more drink than they bought the previous week 
     //      to get some free drinks after specified number of weeks
     //  @return: int (sum from 1 to numWeeks)
@@ -42,5 +45,26 @@ public class CafeUtil {
             sum += price;                           // Add each price to the sum
         }
         return sum;                                 // Return the sum total
+    }
+
+    //  //// Display Menu  ///////////////////////////////////////////
+    //      Given an ArrayList of menu items (strings), print out each index and menu item.
+    //  @param: ArrayList<String> menuItems
+    public void displayMenu(ArrayList<String> menuItems) {
+        for ( int i=0; i<menuItems.size(); i++) {
+            System.out.printf("%d %s\n", i, menuItems.get(i));
+        }
+    }
+
+    //  //// Add Customer  ///////////////////////////////////////////
+    //      Add a customer to the customers in the queue
+    //      then display how many people are ahead in the queue
+    //      then display the entire queue
+    public void addCustomer(ArrayList<String> customersInLine) {
+        // System.out.println("Please enter your name: ");
+
+        String userName = System.console().readLine("Please, enter your name: ");   // Get the User name from the user on the console
+        System.out.printf("Hello, %s!\n", userName);
+
     }
 }
